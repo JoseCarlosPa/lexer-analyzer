41aec6e78e922d5f685bef0960eaa99df628d937
josepachecosanchez <A01702828@tec.mx>
2022-03-17 22:24:09 -0600

    feat(readme&licence): addition of README.md and LICENCE.md and modified setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+sudo apt update
+sudo apt install software-properties-common -y
 
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install python3.9 -y
 
+python3.9 --version
+echo "Instalacion de Python 3.9 Exitoso!! "
 python3 main.py
