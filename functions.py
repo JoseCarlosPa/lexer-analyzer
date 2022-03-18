ef221234e804409e0fd523738f3be5594c646325
josepachecosanchez <A01702828@tec.mx>
2022-03-17 23:29:23 -0600

    feat(missing): addition of separator and comments

--- functions.py
+++ functions.py
@@ -8,27 +8,27 @@ class Color:
 def menu():
     print('-----Menu-----:')
     print('1.- Seleccionar una archivo:')
-    print('2.- Correr pruebas de "test_cases":')
+    print('2.- Usar pruebas de "test_cases":')
     print('3.- Salir')
 
 
 def is_array(str, array):
-    return True if str in array else False
+    return True if str in array else False  # Check if a given string is on the array
 
 
 def read_line(line, term, no_term):
-    stack = []
-    right = False
+    r_side = False
+    pile = []
     for str in line.split():
-        stack.append(str)
-        if str == '->':
-            right = True
-            stack.pop()
-            new_non_terminal = stack.pop()
+        pile.append(str)
+        if str == '->':  # Split operator for right and left ->
+            r_side = True
+            pile.pop()
+            new_non_terminal = pile.pop()
             if not is_array(new_non_terminal, no_term):
                 no_term.append(new_non_terminal)
             term = rm_from_terminals(new_non_terminal, term)
-        elif right and str != "'":
+        elif r_side and str != "'":  # Not counting case for '
             if not is_array(str, no_term) and not is_array(str, term):
                 term.append(str)
     return term, no_term
@@ -53,18 +53,17 @@ def read_process():
 
         file = open(filename, "r")
 
-        nProductions = int(file.readline())
+        product = int(file.readline())
+        terminals = []
+        non_terminals = []
 
-        Terminals = []
-        NonTerminals = []
-
-        for _ in range(nProductions):
+        for _ in range(product):
             line = file.readline()
-            read_line(line, Terminals, NonTerminals)
+            read_line(line, terminals, non_terminals)
 
         print('\n')
-        output_array(Terminals, True)
-        output_array(NonTerminals, False)
+        output_array(terminals, True)
+        output_array(non_terminals, False)
         file.close()
         print('\n')
     except OSError as err:
